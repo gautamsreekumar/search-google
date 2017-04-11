faee1c59fa6ba2af786efeafa219005b52f477f8
gautamsreekumar <gautamsree@gmail.com>
2017-04-11 23:20:15 +0530

    this can now open a website from the command line. it doesn't make sense because opening a website using firefox is still a one-line bash script. i am slowly modifying the code one by one.

--- myCommand.py
+++ myCommand.py
@@ -3,4 +3,5 @@
 import sys
 from subprocess import call
 
-call("firefox https://www.", str(sys.argv)[1], ".com/")
+call("firefox https://www." + sys.argv[1] + ".com", shell = True)
+# print str(sys.argv[1])
---
+++ myCommand.py~
@@ -0,0 +1,6 @@
+#!/usr/bin/python 
+
+import sys
+from subprocess import call
+
+call("firefox https://www.", str(sys.argv)[1], ".com/")
